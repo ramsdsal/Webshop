cb01fe166bd791f4d6042c0ceb805ce04ebd9f9b
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2018-12-02 16:20:06 +0100

    You can now get the price from GET methode in product controller

--- ClientApp/src/components/ProductListItem/ProductListItem.js
+++ ClientApp/src/components/ProductListItem/ProductListItem.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 // import { Product } from "ClientApp/Models/Product";
 import "./ProductListItem.css";
-import { Button, Card, Icon, Image, Grid } from "semantic-ui-react";
+import { Button, Card, Icon, Image, Grid, Label } from "semantic-ui-react";
 
 export default class ProductListItem extends React.Component
 {
@@ -38,12 +38,18 @@ export default class ProductListItem extends React.Component
                 <Card.Header>{movie.title} ({movie.year})</Card.Header>
               </Card.Content>
               <Card.Content extra textAlign="right">
-                <Button>
+
+                  <Card.Content color="black" extra textAlign="left"> 
+                    <Label color="black">Price: {movie.price}</ Label>
+                  </Card.Content>    
+
+                  <Button>
                   <Icon name="cart" />
                 </Button>
                 <Button>
                   <Icon name="heart" />
                 </Button>
+
               </Card.Content>
             </Card>
         </Grid.Column>
