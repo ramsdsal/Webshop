6c2abd3a4a13f3d12508ee3d8c90edd56240395f
RoshniUdh <38688509+RoshniUdh@users.noreply.github.com>
2018-12-05 19:35:35 +0100

    Statistics file correction

--- ClientApp/src/components/Pages/Statistics/Statistics.js
+++ ClientApp/src/components/Pages/Statistics/Statistics.js
@@ -5,50 +5,14 @@ import "./Statistics.css";
 
 export class Statistics extends Component {
 
-  constructor()
-  {
-    super();
-    this.state = { isLoading : "isLoading", movies : [] };
 
-    fetch("/api/Product/GetAdminProducts/1/4")
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        this.setState({ ...this.state, movies : data.items, isLoading : "Not needed"})
-      });
-  }
-
-  renderProductTable()
-  {
-    return <table className="table">
-    <thead>
-      <tr>
-        <th scope="col">Id</th>
-        <th scope="col">Titel</th>
-        <th scope="col">Prijs</th>
-        <th scope="col">Voorraad</th>
-      </tr>
-    </thead>
-    <tbody>
-      {this.state.movies.map(movie =>
-        <tr key = { movie.id}>
-          <td>{movie.id}</td>
-          <td>{movie.title}</td>
-          <td>{movie.price}</td>
-          <td>{movie.quantity}</td>
-        </tr>
-      )}
-    </tbody>
-  </table>
-  }
 
   render() {
-    let contents = this.renderProductTable()
+   
     return (
       <Container style={{ marginTop: "7em" }}>
         <h1>Admin</h1>
-        <Link className="nav-link" to={"/addproduct"}>Voeg Product</Link>
-        { contents }
+        
       </Container>
     );
   }
