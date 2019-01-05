9634cc21afd6903ae26fd8bd2b27ad258eb6ea44
RoshniUdh <38688509+RoshniUdh@users.noreply.github.com>
2019-01-05 19:30:36 +0100

    wachtwoord updaten toegevoegd

--- ClientApp/src/components/Pages/Password/Password.js
+++ ClientApp/src/components/Pages/Password/Password.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
 import { Link} from "react-router-dom";
-import { Container, Grid, Segment, Header, Menu, Form, Input , Button} from 'semantic-ui-react';
+import { Container, Grid, Segment, Header, Menu, Form, Message} from 'semantic-ui-react';
 
 
 export class Password extends Component{
-    constructor(props)
-    {
-        super(props);
-        this.state = { activeItem: 'Wachtwoord wijzigen'  }
+  constructor(props) {
+      super(props);
+  
+      this.state = {
+        isLoading: true,   
+        activeItem: "Wachtwoord wijzigen", 
         
-    }    
-
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-
+        password: "",
+  
+        updateUserError: false,
+        userUpdated: false,
+        serverUpdateUserResponse: "",
+        userFormIsLoading: false,
+  
+      };
+    }
+  
+    componentDidMount() {
+      fetch("/api/user/GetUserById/" + this.props.userId)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          const user = data[0];
+          this.setState({
+            ...this.state,
+            isLoading: false,
+            password: user.password
+          });
+        });
+    }
+  
+    handleChange = (e, { name, value }) => this.setState({ [name]: value });
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name }) 
+  
+    updateUser = () => {
+      this.setState({...this.state, userFormIsLoading: true})
+      var jsonToSend = {
+        id: this.props.userId,
+        password: this.state.password
+      };
+  
+      fetch("/api/user/password", {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jsonToSend)
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({...this.state, serverUpdateUserResponse: data.response, updateUserError: data.isError, userUpdated: data.userUpdated, userFormIsLoading: false})
+      });
+    };
+  
+  
     render() {
-        const { activeItem } = this.state
-        
-        return (
-        <Container style={{ marginTop: "7em" }}>
-        <Grid>
+      const { activeItem } = this.state
+
+      return (
+          <Container style={{ marginTop: "7em" }}>
+          <Grid>
             <Grid.Column width={4}>
             <Header as='h2' attached='top'>
             Instellingen
@@ -36,26 +82,52 @@ export class Password extends Component{
                 <Header as='h2' attached='top'>
                 {this.state.activeItem}
                 </Header>
-                <Segment attached>
-                <Form>
-                    <Form.Group>
-                        <Form.Field>
-                        <h6>Wachtwoord</h6>
-                        <Input type="password"placeholder="********"/>
-                        </Form.Field>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Field>
-                        <h6>Wachtwoord herhaling</h6>
-                        <Input type="password" placeholder="********"/>
-                        </Form.Field>
-                    </Form.Group>
-                    <Button>Opslaan</Button>
-                </Form>
-                </Segment>
-            </Grid.Column>
-        </Grid>
-        </Container>
-        )
+          <Segment attached>
+          <Form 
+            onSubmit={this.updateUser}
+            loading = {this.state.userFormIsLoading}
+            error={this.state.updateUserError}
+            success={this.state.userUpdated}
+          >
+          {
+              this.state.updateUserError ? <Message size='large' error 
+              header="Gebruiker kon niet geupdate worden" 
+              content={this.state.serverUpdateUserResponse}/>
+              :
+              null
+          }
+          {
+              this.state.userUpdated ? <Message size='large' success 
+              header="Gebruiker succesvol aangepast" 
+              content={this.state.serverUpdateUserResponse}/>
+              : 
+              null
+          }
+            <Form.Group unstackable widths={2}>
+              <Form.Input
+                type="password"
+                size="massive"
+                label="Nieuw wachtwoord"
+                placeholder="*****"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Button
+              type='submit'
+              disabled={
+                !this.state.password
+              }
+            >
+             Wijzigen
+            </Form.Button>
+          </Form>
+      </ Segment>
+      </Grid.Column>
+      </Grid>
+  </Container>
+      );
     }
-}
+  }
+    
